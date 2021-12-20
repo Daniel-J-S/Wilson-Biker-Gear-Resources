16a642c1800a39e1c07870d3eb4d7cdc27d21c9c
Daniel JS <daniel@mydeveloperjourney.com>
2021-12-20 17:45:06 -0600

    wip: makes initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export default function Header(props) {
+    return (
+        <header className="Header">
+            <h1>WBG Resources</h1>
+        </header>
+    );
+};
