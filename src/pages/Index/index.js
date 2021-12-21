b017208f430b8393ca2462996fde6c4e4c193cfc
myDeveloperJourney <daniel@mydeveloperjourney.com>
2021-12-20 19:38:46 -0600

    wip: adds updates to site

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,9 +1,30 @@
 import './Index.css';
+import data from '../../data/resources.json';
 
 export default function Index(props) {
+    const indexItems = data.map(d => (
+        <article>
+            <h3>{d.title}</h3>
+            <div className="iframe-container">
+                <iframe 
+                    width="560" 
+                    height="315" 
+                    src="https://www.youtube.com/embed/U3SPkP4y-rY" 
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen 
+                />
+            </div>
+        </article>
+    ))
+
     return (
         <main className="Index">
-            <h1>Index</h1>
+            <h1>Site Resources</h1>
+            <section>
+                {indexItems}
+            </section>
         </main>
     );
 };
