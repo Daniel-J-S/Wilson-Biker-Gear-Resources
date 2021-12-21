dcd4a73d8d514f9a544e216d1b475e6e94bb9855
Daniel JS <daniel@mydeveloperjourney.com>
2021-12-20 18:09:16 -0600

    wip: further builds index page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,47 @@
+import { useState } from 'react';
 import './Home.css';
 
 export default function Home(props) {
+    const [ state, setState ] = useState(getInitialState());
+
+    function getInitialState() {
+        return {
+            email: '',
+            password: '',
+        }
+    }
+
+    const handleChange = e => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setState(getInitialState());
+    };
+
     return (
         <main className="Home">
-            <h1>Home</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Email
+                    <input 
+                        type="email" 
+                        name="email"
+                        onChange={handleChange} 
+                    />
+                </label>
+                <label>Password
+                    <input 
+                        type="password" 
+                        name="password"
+                        onChange={handleChange} 
+                    />
+                </label>
+                <input type="submit" value="Login" />
+            </form>
         </main>
     );
 };
